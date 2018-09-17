90c3997343c9463abec2354a2b3a7836a0d5e9f4
Sam Bahta <smabahta37@gmail.com>
2018-09-17 16:27:33 -0500

    add rest of slider

--- src/components/DailyCheckin/StepSliders/StepSlider.js
+++ src/components/DailyCheckin/StepSliders/StepSlider.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/lab/Slider';
@@ -13,6 +13,8 @@ class StepSlider extends Component {
   state = {
     mood: 3,
     hydro: 3,
+    fitness: 3,
+    nutrition:3,
   };
 
   handleMoodChange = (event, mood) => {
@@ -21,18 +23,28 @@ class StepSlider extends Component {
   };
   handleHydroChange = (event, hydro) => {
     this.setState({ hydro });
-    console.log('slide hydro',hydro);
+    console.log('slide hydro', hydro, this.state);
+  };
+  handleFitnessChange = (event, fitness) => {
+    this.setState({ fitness });
+    console.log('slide fitness', fitness);
+  };
+  handleNutritionChange = (event, nutrition) => {
+    this.setState({ nutrition });
+    console.log('slide nutrition', nutrition);
   };
 
   render() {
     const { classes } = this.props;
-   // const { value } = this.state;
+    // const { value } = this.state;
 
     return (
       <div className={classes.root}>
         <Slider value={this.state.mood} min={0} max={5} step={1} onChange={this.handleMoodChange} />
         <Slider value={this.state.hydro} min={0} max={5} step={1} onChange={this.handleHydroChange} />
-        
+        <Slider value={this.state.fitness} min={0} max={5} step={1} onChange={this.handleFitnessChange} />
+        <Slider value={this.state.nutrition} min={0} max={5} step={1} onChange={this.handleNutritionChange} />
+
       </div>
     );
   }
---
+++ src/redux/reducers/DailyReducer.js
@@ -0,0 +1,9 @@
+
+
+const dailyreducer = (state = null, action) =>{
+    if (action.type = SET_MOOD){
+        return action.payload;
+    } else {
+
+    }
+}
