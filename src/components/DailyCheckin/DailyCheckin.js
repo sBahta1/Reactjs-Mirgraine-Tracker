24c6971a549d89ec3c152ee41f2078427a60f8d0
Sam Bahta <smabahta37@gmail.com>
2018-09-17 13:45:28 -0500

    changed queryies in router to match database/ createddaily checkin.js

---
+++ src/components/DailyCheckin/DailyCheckin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DailyCheckin extends Component {
+
+    render() {
+
+        return (
+            <div></div>
+        )//end return
+    }//end render
+}//end class DailyCheckin
+
+export default DailyCheckin;
